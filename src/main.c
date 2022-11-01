d9682eee354b6c9f3bc60674a3600b89a9f797c5
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-10-31 22:37:56 -0300

    fix: keyboard and clean screen

--- src/main.c
+++ src/main.c
@@ -9,8 +9,8 @@ int *img;
 int xpm1_x;
 int xpm1_y;
 
-int *x;
-int *y;
+int x;
+int y;
 
 
 int create_img(int px, int py)
@@ -18,7 +18,7 @@ int create_img(int px, int py)
 	img = mlx_xpm_file_to_image(m, "open.xpm", &xpm1_x, &xpm1_y);
 	if (img == NULL)
 		return (1);
-	mlx_put_image_to_window(m, w, img, px, py);
+	mlx_put_image_to_window(m, w, img, x, y);
 	return 0;
 }
 
@@ -26,41 +26,41 @@ int	keyboard(int key,void *p)
 {
 	if (key==65307)
 		exit(0);
+	// up
 	if (key==65362)
 	{
-		printf("Key UP\n");
-		mlx_destroy_image(m, img);
-		y -= 1;
-		create_img(x,y);
+		mlx_clear_window(m, w);
+		y-=10;
+		create_img(x, y);
 	}
+	// down
 	if (key==65364)
 	{
-		mlx_destroy_image(m, img);
-		printf("Key DOWN\n");
-		y += 1;
-		create_img(x,y);
+		mlx_clear_window(m, w);
+		y+=10;
+		create_img(x, y);
 	}
+	// left
 	if (key==65361)
 	{
-		mlx_destroy_image(m, img);
-		printf("Key LEFT\n");
-		x -= 1;
-		create_img(x,y);
+		mlx_clear_window(m, w);
+		x-=10;
+		create_img(x, y);
 	}
+	// right
 	if (key==65363)
 	{
-		mlx_destroy_image(m, img);
-		printf("Key RIGHT\n");
-		x += 1;
-		create_img(x,y);
+		// mlx reload window
+		mlx_clear_window(m, w);
+		x+=10;
+		create_img(x, y);
 	}
+	printf("key = %d\n",key);
 	return (0);
 }
 
 int main()
 {
-
-
 	m = mlx_init();
 	w = mlx_new_window(m, 250, 250, "test");
 	if (m == NULL || w == NULL)
