a286e83ad16ffaf096f90fe8550579aa146a6e7f
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-12-14 23:52:22 -0300

    feat: add algorithm to valid path point P to E

--- src/check_path.c
+++ src/check_path.c
@@ -53,6 +53,8 @@ int is_valid_dfs(int column, int line, int *trigger, t_map *map)
 	is_valid_dfs(column - 1, line, trigger, map);
 	is_valid_dfs(column, line + 1, trigger, map);
 	is_valid_dfs(column, line - 1, trigger, map);
+	if (*trigger == 1)
+		return (1);
 	return (0);
 }
 
@@ -61,14 +63,17 @@ int check_path(t_game *game)
 	int line;
 	int column;
 	int trigger;
+	int check;
 
 	trigger = 0;
+	check = 0;
 	t_map *map;
 	map = game->map;
 	column = game->player->x;
 	line = game->player->y;
 
-	if (is_valid_dfs(column, line, &trigger, map) == 1)
+	check = is_valid_dfs(column, line, &trigger, map);
+	if (check == 1)
 		return (0);
 	return (1);
 }
