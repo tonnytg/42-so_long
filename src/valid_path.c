a8f6b05ead3a95e0725ea8f05c1b48b645fcb27b
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-12-22 19:27:03 -0300

    feat: add check function to check dfs function

--- src/valid_path.c
+++ src/valid_path.c
@@ -46,3 +46,22 @@ int	is_valid_dfs(int column, int line, int *trigger, t_map *map)
 		return (1);
 	return (0);
 }
+
+int	check_path(t_game *game)
+{
+	int		line;
+	int		column;
+	int		trigger;
+	int		check;
+	t_map	*map;
+
+	trigger = 0;
+	check = 0;
+	map = game->map;
+	column = game->player->x;
+	line = game->player->y;
+	check = is_valid_dfs(column, line, &trigger, map);
+	if (check == 1)
+		return (0);
+	return (1);
+}
