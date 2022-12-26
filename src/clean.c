471baebd3d3678209022ec45f3e44b3dd460eb82
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-12-26 09:57:15 -0300

    feat: segregate clean functions

---
+++ src/clean.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 13:48:03 by antthoma          #+#    #+#             */
+/*   Updated: 2022/12/28 12:25:18 by antthoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	clean_game(t_game *game, int trigger)
+{
+	if (trigger == 1)
+		game->mlx = malloc(sizeof(void *));
+	if (trigger == 2)
+	{
+		mlx_destroy_image(game->mlx, game->images->wall);
+		mlx_destroy_image(game->mlx, game->images->player);
+		mlx_destroy_image(game->mlx, game->images->collectible);
+		mlx_destroy_image(game->mlx, game->images->exit);
+		trigger = 3;
+	}
+	if (trigger == 3)
+	{
+		mlx_destroy_window(game->mlx, game->window);
+		mlx_destroy_display(game->mlx);
+		zero_map_location(game);
+	}
+	free(game->map);
+	free(game->player);
+	free(game->images);
+	free(game->mlx);
+	free(game);
+	return (0);
+}
