54f0b20b9b4dcb1d57607e92c64c59ee9aa6ddf4
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-12-30 10:11:45 -0300

    feat: update all prototypes

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 13:48:03 by antthoma          #+#    #+#             */
-/*   Updated: 2022/12/10 18:13:38 by antthoma         ###   ########.fr       */
+/*   Updated: 2022/12/29 00:32:08 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,26 @@
 # define BUFFER_SIZE 42
 # define PIXEL_SIZE 32
 
-#include <mlx.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+# include <mlx.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
+# include <string.h>
 
 typedef struct s_map
 {
-	int		fd;
-	int		**location;
-	int		count_lines;
-	int		count_columns;
-	int		count_collectibles;
-	int 	count_exits;
-	int		count_player;
-	int		count_walls;
-	int		count_floors;
-	int		count_wrong_c;
+	int	fd;
+	int	**location;
+	int	count_lines;
+	int	count_columns;
+	int	count_collectibles;
+	int	count_exits;
+	int	count_zeros;
+	int	count_walls;
+	int	count_wrong_c;
+	int	count_player;
 }	t_map;
 
 typedef struct s_images
@@ -66,9 +67,16 @@ typedef struct s_game
 	void			*window;
 	int				width;
 	int				height;
-	int 			screen_width;
-	int 			screen_height;
+	int				screen_width;
+	int				screen_height;
 	int				c_collectible;
+	int				x;
+	int				error;
+	int				error_c;
+	int				trigger;
+	char			*content;
+	int				line;
+	int				column;
 	struct s_images	*images;
 	struct s_map	*map;
 	struct s_player	*player;
@@ -82,11 +90,11 @@ int		ft_len_digits(unsigned int n);
 int		ft_is_negative(int n);
 char	*ft_itoa(int n);
 char	*get_next_line(int fd);
+int		ft_putstr(char *str);
 
 int		load_images(t_game *game);
 int		build_map(t_game *game);
 int		load_map(char **argv, t_game *game);
-int		build_display_movement(t_game *game);
 int		key_press(int keycode, t_game *game);
 int		destroy_window(t_game *game);
 int		build_window(t_game *game);
@@ -94,8 +102,19 @@ void	put_image(t_game *game, int *image, int x, int y);
 int		read_map_file(t_game *game, char **argv);
 int		check_rules(t_game *game);
 int		clean_game(t_game *game, int trigger);
+int		msg_error(t_game *game, char *msg, int trigger);
+int		check_format_file(int argc, char **argv);
+int		check_map_walls(t_game *game);
 int		check_path(t_game *game);
-int		readMap(t_map *map);
-void	expose_hook(t_game *game);
+int		expose_hook(t_game *game);
+int		zero_map_location(t_game *game);
+int		build_display_movement(t_game *game);
+t_game	*init_game(t_game *game);
+void	load_events(t_game *game);
+int		msg_error(t_game *game, char *msg, int trigger);
+void	empty_get_next_line(t_game *game);
+int		get_content_file(t_game *game);
+int		map_count_itens(t_game *game, char *line, int x);
+int		loop_content_map(t_game *game);
 
 #endif //SO_LONG_H
